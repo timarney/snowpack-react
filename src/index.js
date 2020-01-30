4715290ce8ea31991e77b145f73ed011aedb9118
Tim Arney <tim@line37.com>
2020-01-30 11:08:19 -0500

    fix import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// importing from ./components/App gets blocked
-// Firefox and Chrome
-// 👇👇👇👇👇
-// Loading module from “http://localhost:8080/lib/components/App” was blocked because of a disallowed MIME type (“text/html”).
-
-import { App } from "./components/App";
-
-/*
-const App = () => {
-  return <div>Hello from React</div>;
-};
-*/
+import { App } from "./components/App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
